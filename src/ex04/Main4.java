26ff44f5e056401a1cca3f1da66b3d66d6affe3d
26077914 <emersonrolim@gmail.com>
2019-02-15 20:58:27 -0300

    Loop while

--- src/ex04/Main4.java
+++ src/ex04/Main4.java
@@ -3,21 +3,22 @@ package ex04;
 import java.util.Scanner;
 
 public class Main4 {
-    public static void main (String args []){
+    public static void main (String args []) {
         Scanner s = new Scanner(System.in);
+        int valor = 0;
+        while (valor <= 100) {
+            System.out.print("Informar o Valor: ");
+            valor = s.nextInt();
 
-        System.out.print("Informar o Valor: ");
-       int valor = s.nextInt();
-
-       if
-       ((valor >=0) && (valor<=100)){
+            if
+            ((valor >= 0) && (valor <= 100)) {
                 System.out.println("Esta dendro da Faixa!");
-            }else{
+            } else {
                 System.out.println("Fora da Faixa");
             }
 
         }
 
-
+    }
     }
 
